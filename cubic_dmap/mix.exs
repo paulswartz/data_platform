ba71dd7a822fa280c2df9b8f0495fb3a4f438bef
Paul Swartz <paul@paulswartz.net>
2022-03-15 11:33:34 -0400

    feat: CI for cubic_dmap

--- cubic_dmap/mix.exs
+++ cubic_dmap/mix.exs
@@ -7,14 +7,17 @@ defmodule CubicDmap.MixProject do
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [
+        tool: LcovEx
+      ]
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :mix]
     ]
   end
 
@@ -23,7 +26,10 @@ defmodule CubicDmap.MixProject do
     [
       {:mojito, "~> 0.7"},
       {:jason, "~> 1.3"},
-      {:explorer, "~> 0.1.0-dev", github: "elixir-nx/explorer", branch: "main"}
+      {:explorer, "~> 0.1.0-dev", github: "elixir-nx/explorer", branch: "main"},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.1", only: :dev, runtime: false},
+      {:lcov_ex, "~> 0.2", only: [:dev, :test], runtime: false}
     ]
   end
 end
